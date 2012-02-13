eed161e4121b7724772fbcfe4700fda3ee56b622
Sunimal Rathnayake <sunimalr@gmail.com>
2012-02-14 01:37:43 +0530

    14 feb 2012

---
+++ src/com/mediplus/presentation/MedicalHistory.java
@@ -0,0 +1,104 @@
+package com.mediplus.presentation;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MedicalHistory extends Activity{
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TableLayout table = new TableLayout(this);
+
+        table.setStretchAllColumns(true);
+        table.setShrinkAllColumns(true);
+
+        TableRow rowTitle = new TableRow(this);
+        rowTitle.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        TableRow rowDayLabels = new TableRow(this);
+        TableRow rowHighs = new TableRow(this);
+        TableRow rowLows = new TableRow(this);
+        TableRow rowConditions = new TableRow(this);
+        rowConditions.setGravity(Gravity.CENTER);
+
+        TextView empty = new TextView(this);
+        empty.setText("Date");
+
+        // title column/row
+        TextView title = new TextView(this);
+        title.setText("Java Weather Table");
+
+        title.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
+        title.setGravity(Gravity.CENTER);
+        title.setTypeface(Typeface.SERIF, Typeface.BOLD);
+
+        TableRow.LayoutParams params = new TableRow.LayoutParams();
+        params.span = 2;
+
+        rowTitle.addView(title, params);
+
+        // labels column
+        TextView highsLabel = new TextView(this);
+        highsLabel.setText("Day High");
+        highsLabel.setTypeface(Typeface.DEFAULT_BOLD);
+
+        TextView lowsLabel = new TextView(this);
+        lowsLabel.setText("Day Low");
+        lowsLabel.setTypeface(Typeface.DEFAULT_BOLD);
+
+        TextView conditionsLabel = new TextView(this);
+        conditionsLabel.setText("Conditions");
+        conditionsLabel.setTypeface(Typeface.DEFAULT_BOLD);
+
+        rowDayLabels.addView(empty);
+        rowHighs.addView(highsLabel);
+        rowLows.addView(lowsLabel);
+        rowConditions.addView(conditionsLabel);
+
+        
+        
+
+        // day4 column
+        TextView day4Label = new TextView(this);
+        day4Label.setText("Feb 10");
+        day4Label.setTypeface(Typeface.SERIF, Typeface.BOLD);
+
+        TextView day4High = new TextView(this);
+        day4High.setText("17°F");
+        day4High.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        TextView day4Low = new TextView(this);
+        day4Low.setText("5°F");
+        day4Low.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        ImageView day4Conditions = new ImageView(this);
+        day4Conditions.setImageResource(R.drawable.user_anonymous);
+
+        rowDayLabels.addView(day4Label);
+        rowHighs.addView(day4High);
+        rowLows.addView(day4Low);
+        rowConditions.addView(day4Conditions);
+
+       
+        table.addView(rowTitle);
+        table.addView(rowDayLabels);
+        table.addView(rowHighs);
+        table.addView(rowLows);
+        table.addView(rowConditions);
+
+        setContentView(table);
+
+    }
+
+
+}
---
+++ src/com/mediplus/presentation/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.mediplus.presentation;
+
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splashscreen);
+		Thread timer=new Thread(){
+			public void run(){
+				try{
+					sleep(4000);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent openMenu=new Intent("com.mediplus.presentation.MENU");
+					startActivity(openMenu);
+				}
+				
+			}
+		};
+		timer.start();
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
+	
+	
+	
+}
